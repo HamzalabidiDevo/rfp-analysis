f298a9ed5e61968e43b0673d7e466acee6a4a4e7
HamzalabidiDevo <hamza.labidi@devoteam.com>
2024-10-03 13:39:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ const server = http.createServer((req, res) => {
     res.end('Hello Awesome')
 }
                  )
-
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
